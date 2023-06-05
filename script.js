fb67c6e64b976aae6510de5485516ae224ae0e32
jhood34 <thejhood34@gmail.com>
2023-06-05 13:17:20 +1000

    Update script.js

--- script.js
+++ script.js
@@ -29,6 +29,29 @@ if (document.body.classList.contains("landing-page")) {
 }
 
 
+// Remove hover effects on mobile ()
+function hasTouch() {
+  return 'ontouchstart' in document.documentElement
+         || navigator.maxTouchPoints > 0
+         || navigator.msMaxTouchPoints > 0;
+}
+
+if (hasTouch()) { // remove all the :hover stylesheets
+  try { // prevent exception on browsers not supporting DOM styleSheets properly
+    for (var si in document.styleSheets) {
+      var styleSheet = document.styleSheets[si];
+      if (!styleSheet.rules) continue;
+
+      for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+        if (!styleSheet.rules[ri].selectorText) continue;
+
+        if (styleSheet.rules[ri].selectorText.match(':hover')) {
+          styleSheet.deleteRule(ri);
+        }
+      }
+    }
+  } catch (ex) {}
+}
 
 // Live clock functionality (3)
 
